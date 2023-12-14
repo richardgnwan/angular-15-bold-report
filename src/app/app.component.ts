aa6688e5297519642359678215b63bc948591355
Richard Gunawan <richard.gunawan@gdplabs.id>
2023-12-14 17:39:51 +0700

    feat: implement designer to edit report

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,76 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  private readonly TOKEN: string = 'YOUR_TOKEN_HERE';
-  private readonly SERVER_URL: string = 'YOUR_SERVER_URL';
+  private readonly TOKEN: string = environment.TOKEN;
+  private readonly SERVER_URL: string = environment.SERVER_URL;
 
-  // ReportServer Integration - Demo
+  constructor() {}
+
+  /* VARIABLES */
+
+  /* ReportServer Integration - Demo */
   // serviceUrl: string = 'https://demos.boldreports.com/services/api/ReportingAPI';
 
-  // ReportServer Integration - Cloud
+  /* ReportServer Integration - Cloud */
   // reportServerUrl: string = this.SERVER_URL + '/reporting/api';
   // serviceUrl: string = 'https://service.boldreports.com/api/Designer';
   // serverServiceAuthorizationToken: string = this.TOKEN;
 
-  // ReportViewer Integration
-  reportServerUrl: string = this.SERVER_URL + '/reporting/api/';
-  reportServiceUrl: string = 'https://service.boldreports.com/api/Viewer';
-  serverServiceAuthorizationToken: string = this.TOKEN;
-  reportPath: string = '/Sample Reports/Sales Order Detail';
+  /* ReportViewer Integration */
+  // reportServerUrl: string = this.SERVER_URL + '/reporting/api/';
+  // reportServiceUrl: string = 'https://service.boldreports.com/api/Viewer';
+  // serverServiceAuthorizationToken: string = this.TOKEN;
+  // reportPath: string = '/Sample Reports/Sales Order Detail';
+
+
+
+  /* INTEGRATIONS */
+  /**
+   * Not working
+   * Using JQuery: https://help.boldreports.com/embedded-reporting/javascript-reporting/report-designer/how-to/open-server-report-using-report-path-on-opening-designer/
+   */
+  // ngOnInit(): void {
+  //   $(function () {
+  //     $("#designer").boldReportDesigner({
+  //       serviceUrl: 'https://service.boldreports.com/api/Designer',
+  //       reportServerUrl: environment.SERVER_URL + '/reporting/api',
+  //       serviceAuthorizationToken: environment.TOKEN
+  //     });
+  //     var designerObj = $('#designer').data('boldReportDesigner');
+  //     designerObj.openReport('/Sample Reports/Sales Order Detail');
+  //   })
+  // }
+
+  /**
+   * Not working
+   * Using @ViewChild
+   */
+  // ngAfterViewInit(): void {
+  //   this.initializeBoldReportsDesigner();
+  // }
+
+  // private initializeBoldReportsDesigner() {
+  //   const designerObject: any =
+  //     this.reportDesigner.nativeElement['boldReportDesigner'];
+  //   if (designerObject) {
+  //     console.log('openning report');
+  //     designerObject.openReport('/Sample Reports/Sales Order Detail');
+  //   } else {
+  //     console.error('Bold Reports Designer object not found.');
+  //   }
+  // }
 
 }
